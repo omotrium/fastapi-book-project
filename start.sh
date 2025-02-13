8ef09aa480b47d1b5f5ffa8e747482ccab413cf4
Talabi Omotayo <omo4adex@gmail.com>
2025-02-13 14:42:06 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update & Install Nginx
+apt-get update && apt-get install -y nginx
+
+# Remove default config if needed
+rm -f /etc/nginx/sites-enabled/default
+
+# Create Nginx Config
+cat <<EOF > /etc/nginx/sites-available/app
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}
+EOF
+
+# Enable Nginx Config
+ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled/
+
+# Restart Nginx
+service nginx restart
+
+# Start Python App
+uvicorn main:app --host 127.0.0.1 --port 8000

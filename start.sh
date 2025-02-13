02e6b844c41cf67b2de135cf075c4c3bdeaf9762
Talabi Omotayo <omo4adex@gmail.com>
2025-02-13 14:51:56 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -28,4 +28,4 @@ ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled/
 service nginx restart
 
 # Start Python App
-uvicorn main:app --host 127.0.0.1 --port 8000
+uvicorn main:app --host 0.0.0.0 --port $PORT
